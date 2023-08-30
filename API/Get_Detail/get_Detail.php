d08d5e5f9b99cb63e9cd45b2dceb6a32842ee51b
SyikhaSonOfAdin <syikhasmart@gmail.com>
2023-08-30 13:06:41 +0700

    Balance update

--- API/Get_Detail/get_Detail.php
+++ API/Get_Detail/get_Detail.php
@@ -11,8 +11,8 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         $table = "data_mir";
         $query = "SELECT * FROM $table WHERE IDENT_CODE = '$ic' ORDER BY batch DESC";
         $conn = conn();
-    
-    
+
+
         $data .= '<table class="w-full text-sm text-left text-gray-500">' .
             '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
             '<tr>' .
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             '</tr>' .
             '</thead>' .
             '<tbody>';
-        $i = 1 ; 
+        $i = 1;
         $result = mysqli_query($conn, $query);
         while ($raw = mysqli_fetch_assoc($result)) {
             $data .= '<tr class="bg-white border-b">' .
@@ -33,9 +33,9 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 '<td class="px-6 py-4">' . $raw["spool"] . '</td>' .
                 '<td class="px-6 py-4">' . $raw["IDENT_CODE"] . '</td>' .
                 '<td class="px-6 py-4">' . $raw["bm_qty"] . '</td>' . '</tr>';
-                $i++ ;
+            $i++;
         }
-    
+
         $data .= '</tbody>' .
             '</table>';
         echo $data;
@@ -43,8 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         $table = "material_receive_hein";
         $query = "SELECT * FROM $table WHERE IDENT_CODE = '$ic' ORDER BY `mir` DESC, `tanggal` DESC";
         $conn = conn();
-    
-    
+
+
         $data .= '<table class="w-full text-sm text-left text-gray-500">' .
             '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
             '<tr>' .
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             '</tr>' .
             '</thead>' .
             '<tbody>';
-        $i = 1 ;
+        $i = 1;
         $result = mysqli_query($conn, $query);
         while ($raw = mysqli_fetch_assoc($result)) {
             $data .= '<tr class="bg-white border-b">' .
@@ -68,9 +68,9 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 '<td class="px-6 py-4">' . $raw["tanggal"] . '</td>' .
                 '<td class="px-6 py-4">' . $raw["uploader"] . '</td>' .
                 '</tr>';
-                $i++ ;
+            $i++;
         }
-    
+
         $data .= '</tbody>' .
             '</table>';
         echo $data;
@@ -78,8 +78,8 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         $table = "material_used_hein";
         $query = "SELECT * FROM $table WHERE IDENT_CODE = '$ic' ORDER BY `mir` DESC, `date` DESC";
         $conn = conn();
-    
-    
+
+
         $data .= '<table class="w-full text-sm text-left text-gray-500">' .
             '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
             '<tr>' .
@@ -94,7 +94,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             '</tr>' .
             '</thead>' .
             '<tbody>';
-        $i = 1 ;
+        $i = 1;
         $result = mysqli_query($conn, $query);
         while ($raw = mysqli_fetch_assoc($result)) {
             $data .= '<tr class="bg-white border-b">' .
@@ -107,9 +107,9 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 '<td class="px-6 py-4">' . $raw["uploader"] . '</td>' .
                 '<td class="px-6 py-4">' . $raw["bywho"] . '</td>' .
                 '</tr>';
-                $i++ ;
+            $i++;
         }
-    
+
         $data .= '</tbody>' .
             '</table>';
         echo $data;
@@ -118,14 +118,14 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         $query = "SELECT * FROM $table WHERE IDENT_CODE = '$ic' ORDER BY batch DESC";
         $conn = conn();
         $resultData = mysqli_query($conn, $query);
-    
+
         $tempData = [];
-    
+
         $table1 = "material_used_hein";
         $query1 = "SELECT * FROM $table1 WHERE IDENT_CODE = '$ic' ORDER BY `mir` DESC, `date` DESC";
         $conn1 = conn();
         $resultUsed = mysqli_query($conn1, $query1);
-    
+
         while ($rowData = mysqli_fetch_assoc($resultData)) {
             $tempData[] = [
                 "Batch No" => $rowData["batch"],
@@ -135,7 +135,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 "Found" => ""
             ];
         }
-    
+
         $tempUsed = [];
         while ($used = mysqli_fetch_assoc($resultUsed)) {
             $tempUsed[] = [
@@ -146,13 +146,13 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 "Found" => ""
             ];
         }
-    
+
         for ($i = 0; $i < count($tempData); $i++) {
             for ($y = 0; $y < count($tempUsed); $y++) {
                 if ($tempUsed[$y]["Found"] == '') {
                     if (trim($tempData[$i]["Spool No"]) == trim($tempUsed[$y]["Spool No"])) {
                         $tempData[$i]["BM Qty"] -= $tempUsed[$y]["Qty"];
-                        $tempUsed[$y]["Found"] = "Founded" ;
+                        $tempUsed[$y]["Found"] = "Founded";
                         if ($tempData[$i]["BM Qty"] <= 0) {
                             $tempData[$i]["Found"] = "Founded";
                         }
@@ -162,11 +162,11 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         }
         // file_put_contents("issued.json", json_encode($tempUsed));
         // file_put_contents("test.json", json_encode($tempData));
-    
-    
+
+
         $temp = [];
         foreach ($tempData as $item) {
-            if ($item["Found"] == "" && $item["BM Qty"] > 0.09 ) {
+            if ($item["Found"] == "" && $item["BM Qty"] > 0.09) {
                 $temp[] = [
                     "Batch No" => $item["Batch No"],
                     "Spool No" => $item["Spool No"],
@@ -175,7 +175,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
                 ];
             }
         }
-    
+
         $data = '<div class="h-full lg:w-[80%] mr-2"><h1 class="text-red-500 font-bold text-lg">Need Reservation</h1>' .
             '<table class="w-full text-sm text-left text-gray-500 h-max">' .
             '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
@@ -188,126 +188,167 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             '</tr>' .
             '</thead>' .
             '<tbody>';
-        $total = 0 ;
-        $y = 1 ;
-        for ($i = 0 ; $i < count($tempUsed) ; $i++) {
+        $total = 0;
+        $y = 1;
+        for ($i = 0; $i < count($tempUsed); $i++) {
             if ($tempUsed[$i]["Found"] == "") {
-                $total += $tempUsed[$i]["Qty"] ;
+                $total += $tempUsed[$i]["Qty"];
                 $data .= '<tr class="bg-white border-b">' .
-                '<td class="px-6 py-4">' . $y . '</td>' .
-                '<td class="px-6 py-4">' . $tempUsed[$i]["Batch No"] . '</td>' .
-                '<td class="px-6 py-4">' . $tempUsed[$i]["Spool No"] . '</td>' .
-                '<td class="px-6 py-4">' . $tempUsed[$i]["Ident Code"] . '</td>' .
-                '<td class="px-6 py-4">' . round($tempUsed[$i]["Qty"], 2) . '</td>
+                    '<td class="px-6 py-4">' . $y . '</td>' .
+                    '<td class="px-6 py-4">' . $tempUsed[$i]["Batch No"] . '</td>' .
+                    '<td class="px-6 py-4">' . $tempUsed[$i]["Spool No"] . '</td>' .
+                    '<td class="px-6 py-4">' . $tempUsed[$i]["Ident Code"] . '</td>' .
+                    '<td class="px-6 py-4">' . round($tempUsed[$i]["Qty"], 2) . '</td>
                 </tr>';
-                $y++ ;
+                $y++;
             }
         }
         $data .= '<tr class="bg-white border-b">' .
-                '<td class="px-6 py-4">' . 'Total :' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . $total . '</td></tr>
+            '<td class="px-6 py-4">' . 'Total :' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . $total . '</td></tr>
                 </tbody>' .
             '</table></div>';
         if ($y == 1) {
-            $data = '' ;
-            $data .= 
-            '<div class="w-full">
+            $data = '';
+            $data .=
+                '<div class="w-full">
             <table class="w-full text-sm text-left text-gray-500">' .
-            '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
-            '<tr>' .
-            '<th scope="col" class="px-6 py-3">No</th>' .
-            '<th scope="col" class="px-6 py-3">MIR No</th>' .
-            '<th scope="col" class="px-6 py-3">Spool</th>' .
-            '<th scope="col" class="px-6 py-3">Ident Code</th>' .
-            '<th scope="col" class="px-6 py-3">Quantity</th>' .
-            '</tr>' .
-            '</thead>' .
-            '<tbody>';
+                '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
+                '<tr>' .
+                '<th scope="col" class="px-6 py-3">No</th>' .
+                '<th scope="col" class="px-6 py-3">MIR No</th>' .
+                '<th scope="col" class="px-6 py-3">Spool</th>' .
+                '<th scope="col" class="px-6 py-3">Ident Code</th>' .
+                '<th scope="col" class="px-6 py-3">Quantity</th>' .
+                '</tr>' .
+                '</thead>' .
+                '<tbody>';
         } else {
-            $data .= 
-            '<div class="lg:w-[80%] ml-2"><h1 class="font-bold text-lg">Remaining Spool</h1>
+            $data .=
+                '<div class="lg:w-[80%] ml-2"><h1 class="font-bold text-lg">Remaining Spool</h1>
             <table class="w-full text-sm text-left text-gray-500">' .
-            '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
-            '<tr>' .
-            '<th scope="col" class="px-6 py-3">No</th>' .
-            '<th scope="col" class="px-6 py-3">MIR No</th>' .
-            '<th scope="col" class="px-6 py-3">Spool</th>' .
-            '<th scope="col" class="px-6 py-3">Ident Code</th>' .
-            '<th scope="col" class="px-6 py-3">Quantity</th>' .
-            '</tr>' .
-            '</thead>' .
-            '<tbody>';
+                '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
+                '<tr>' .
+                '<th scope="col" class="px-6 py-3">No</th>' .
+                '<th scope="col" class="px-6 py-3">MIR No</th>' .
+                '<th scope="col" class="px-6 py-3">Spool</th>' .
+                '<th scope="col" class="px-6 py-3">Ident Code</th>' .
+                '<th scope="col" class="px-6 py-3">Quantity</th>' .
+                '</tr>' .
+                '</thead>' .
+                '<tbody>';
         }
-    
-        
-        $i = 1 ;
-        $total = 0 ;
+
+
+        $i = 1;
+        $total = 0;
         foreach ($temp as $item) {
-            $total += $item["Qty"] ;
+            $total += $item["Qty"];
             $data .= '<tr class="bg-white border-b">' .
                 '<td class="px-6 py-4">' . $i . '</td>' .
                 '<td class="px-6 py-4">' . $item["Batch No"] . '</td>' .
                 '<td class="px-6 py-4">' . $item["Spool No"] . '</td>' .
                 '<td class="px-6 py-4">' . $item["Ident Code"] . '</td>' .
                 '<td class="px-6 py-4">' . round($item["Qty"], 2) . '</td></tr>';
-                $i++ ;
+            $i++;
         }
         $data .= '<tr class="bg-white border-b">' .
-                '<td class="px-6 py-4">' . 'Total :' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . '</td>' .
-                '<td class="px-6 py-4">' . $total . '</td></tr>';
+            '<td class="px-6 py-4">' . 'Total :' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . '</td>' .
+            '<td class="px-6 py-4">' . $total . '</td></tr>';
         $data .= '</tbody>' .
             '</table></div>';
         echo $data;
-    
+
     } else if ($p == 'balance') {
         $data = [
-            'Sorted' => [] 
-        ] ;
+            'Sorted' => []
+        ];
 
-        $reservation = [] ;
-        $reservationTable = 'data_mir' ;
-        $reservationConn = conn() ;
-        $reservationQuery = "SELECT * FROM $reservationTable WHERE IDENT_CODE = '$ic'" ;
-        $reservationResult = mysqli_query($reservationConn, $reservationQuery) ;
+        $reservation = [];
+        $reservationTable = 'data_mir';
+        $reservationConn = conn();
+        $reservationQuery = "SELECT * FROM $reservationTable WHERE IDENT_CODE = '$ic'";
+        $reservationResult = mysqli_query($reservationConn, $reservationQuery);
         while ($reservationData = mysqli_fetch_assoc($reservationResult)) {
             $reservation[] = [
                 'MIR No' => $reservationData["batch"],
                 'Spool No' => $reservationData["spool"],
                 'MIR Qty' => $reservationData["bm_qty"],
-            ] ;
+            ];
         }
 
-        $receive = [] ;
-        $receiveTable = 'material_receive_hein' ;
-        $receiveConn = conn() ;
-        $receiveQuery = "SELECT * FROM $receiveTable WHERE IDENT_CODE = '$ic'" ;
-        $receiveResult = mysqli_query($receiveConn, $receiveQuery) ;
+        $receive = [];
+        $receiveTable = 'material_receive_hein';
+        $receiveConn = conn();
+        $receiveQuery = "SELECT * FROM $receiveTable WHERE IDENT_CODE = '$ic'";
+        $receiveResult = mysqli_query($receiveConn, $receiveQuery);
         while ($receiveData = mysqli_fetch_assoc($receiveResult)) {
             $receive[] = [
                 'MIR No' => $receiveData["mir"],
                 'Qty' => $receiveData["qty"],
-            ] ;
+            ];
         }
 
-        for ($i = 0 ; $i < count($reservation) ; $i++) {
-            for ($j = 0 ; $j < count($reservation) ; $j++) {
-                if (trim($reservation[$i]["MIR No"]) == trim($reservation[$j]["MIR No"])) {
-                    $data[$i]['Sorted'][] = $reservation[$j] ; 
+        $missingData = [];
+
+        // Loop through reservation data and check if it exists in receive data
+        foreach ($reservation as $reservationItem) {
+            $mirNo = $reservationItem['MIR No'];
+            $foundInReceive = false;
+
+            foreach ($receive as $receiveItem) {
+                if (trim($receiveItem['MIR No']) === trim($mirNo)) {
+                    $foundInReceive = true;
+                    break;
                 }
+            }
+
+            if (!$foundInReceive) {
+                $missingData[] = $reservationItem;
             }
         }
 
-        $jsonReservation = json_encode($data);
+        $data['Sorted'] = $missingData;
 
-        // Menulis JSON ke dalam file
-        $file = 'reservation.json';
-        file_put_contents($file, $jsonReservation);
+        // Membangun string HTML
+        $htmlData = '<div class="h-full w-full mr-2">' .            
+            '<table class="w-full text-sm text-left text-gray-500 h-max">' .
+            '<thead class="text-xs text-gray-700 uppercase bg-gray-50">' .
+            '<tr>' .
+            '<th scope="col" class="px-6 py-3">No</th>' .
+            '<th scope="col" class="px-6 py-3">MIR No</th>' .
+            '<th scope="col" class="px-6 py-3">Spool No</th>' .
+            '<th scope="col" class="px-6 py-3">MIR Quantity</th>' .
+            '</tr>' .
+            '</thead>' .
+            '<tbody>';
+
+        $total = 0;
+        for ($i = 0; $i < count($data['Sorted']); $i++) {
+            $total += $data['Sorted'][$i]['MIR Qty'];
+            $htmlData .= '<tr class="bg-white border-b">' .
+                '<td class="px-6 py-4">' . ($i + 1) . '</td>' .
+                '<td class="px-6 py-4">' . $data['Sorted'][$i]['MIR No'] . '</td>' .
+                '<td class="px-6 py-4">' . $data['Sorted'][$i]['Spool No'] . '</td>' .
+                '<td class="px-6 py-4">' . $data['Sorted'][$i]['MIR Qty'] . '</td>' .
+                '</tr>';
+        }
+
+        $htmlData .= '<tr class="bg-white border-b">' .
+            '<td class="px-6 py-4">' . 'Total :' . '</td>' .
+            '<td class="px-6 py-4" colspan="2"></td>' .
+            '<td class="px-6 py-4">' . $total . '</td></tr>' .
+            '</tbody></table></div>';
+
+        echo $htmlData;
+
+
 
     } else {
         echo $data .= '<h1>Invalid Parameter</h1>';
